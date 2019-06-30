3437db4d84ec888a397906be13c77c03fc42b2e2
wilfison <wilfisonbatista@gmail.com>
2019-06-29 22:22:05 -0300

    Break file VS Code

--- AppInstall.sh
+++ AppInstall.sh
@@ -79,19 +79,7 @@ do
 			xdg-open http://127.0.0.1/;;
 
 
-
-		6)
-			echo -e "$green Installing Access Key! $close_color" &&
-			curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &&
-			sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg &&
-			sudo sh -c 'echo -e "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' &&
-			echo -e "$green Updating... $close_color" &&
-			sudo apt update &&
-			echo -e "$green Installing VS Code $close_color" &&
-			sudo apt install code -y &&
-			echo -e "$green VS Code successfully installed $close_color"
-			code;;
-
+		6) bash $BASEDIR/components/vscode.sh;;
 
 
 		7)
