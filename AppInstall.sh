287a1bbc8f5aa1acef7c022004dc921e0d18c231
wilfison <wilfisonbatista@gmail.com>
2019-07-30 21:28:09 -0300

    Create rails project by template

--- AppInstall.sh
+++ AppInstall.sh
@@ -7,28 +7,27 @@ source $BASEDIR/components/helpers/profile.sh
 while true; do
 
 echo -e "$blue
- =======================================================================
- |                   What are we going to install now?                 |
- =======================================================================
-$close_color
+ =====================================================================================
+ |                         What are we going to install now?                         |
+ ===================================================================================== $close_color
+
 $green Development: $close_color
- 
-$green  1 $close_color ➜ $blue RVM + Rails + PostgreSql $close_color |    $green  2 $close_color ➜ $blue React Native CLI  $close_color
-$green  3 $close_color ➜ $blue Git $close_color                      |    $green  4 $close_color ➜ $blue Electron  $close_color
-$green  5 $close_color ➜ $blue NVM $close_color                      |    $green  6 $close_color ➜ $blue Ionic  $close_color
-$green  7 $close_color ➜ $blue Nodejs $close_color                   |    $green  8 $close_color ➜ $blue My ZSH  $close_color
-$green  9 $close_color ➜ $blue Visual Code $close_color              |    $green 10 $close_color ➜ $blue Open Jdk 8  $close_color
-$green 11 $close_color ➜ $blue Docker  $close_color
+
+  1 ➜ $blue RVM + Rails + PostgreSql $close_color      2 ➜ $blue New Rails Project with Template $close_color
+  3 ➜ $blue Visual Code $close_color                   4 ➜ $blue React Native CLI $close_color
+  5 ➜ $blue Git $close_color                           6 ➜ $blue Electron  $close_color
+  7 ➜ $blue NVM $close_color                           8 ➜ $blue Ionic  $close_color
+  9 ➜ $blue Nodejs $close_color                       10 ➜ $blue My ZSH  $close_color
+ 11 ➜ $blue Docker  $close_color
+
+$green Tools and others: $close_color										
 																																		
-$green Tools and others: $close_color														
-																																		
-$green 50 $close_color ➜ $blue Google Chrome $close_color           
-$green 51 $close_color ➜ $blue Telegram $close_color                
-$green 52 $close_color ➜ $blue Codecs and Extras $close_color
-																																		
-		               0 ➜ Sair  																				
+ 50 ➜ $blue Google Chrome $close_color           
+ 51 ➜ $blue Telegram $close_color                
+ 52 ➜ $blue Codecs and Extras $close_color
+
 $blue
- ====================== Created by Wilfison Batista ===================== $close_color
+ ============================ Created by Wilfison Batista =========================== $close_color
 "
 echo -e "$green Enter the number of components to install by comma-separated: $close_color"
 
@@ -50,23 +49,23 @@ do
 	case $addr in
 		1) bash $BASEDIR/components/rails_rvm.sh;;
 
-		2) bash $BASEDIR/components/react_native.sh;;
+		2) bash $BASEDIR/components/rails_template.sh;;
 
-		3) bash $BASEDIR/components/git.sh;;
+		3) bash $BASEDIR/components/vscode.sh;;
 
-		4) bash $BASEDIR/components/electron.sh;;
+		4) bash $BASEDIR/components/react_native.sh;;
 
-		5) bash $BASEDIR/components/nvm.sh;;
+		5) bash $BASEDIR/components/git.sh;;
 
-		6) bash $BASEDIR/components/ionic.sh;;
+		6) bash $BASEDIR/components/electron.sh;;
 
-		7) bash $BASEDIR/components/node.sh;;
+		7) bash $BASEDIR/components/nvm.sh;;
 
-		8) bash $BASEDIR/components/zsh.sh;;
+		8) bash $BASEDIR/components/ionic.sh;;
 
-		9) bash $BASEDIR/components/vscode.sh;;
+		9) bash $BASEDIR/components/node.sh;;
 
-		10) bash $BASEDIR/components/jdk.sh;;
+		10) bash $BASEDIR/components/zsh.sh;;
 
 		11) bash $BASEDIR/components/docker.sh;;
 
