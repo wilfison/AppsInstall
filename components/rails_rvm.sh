50f694d180f0f7e6fcc7d3ac44aca7fc7f572b31
wilfison <wilfisonbatista@gmail.com>
2019-08-02 22:28:05 -0300

    fix dialog in rails installer

--- components/rails_rvm.sh
+++ components/rails_rvm.sh
@@ -2,36 +2,33 @@
 
 source $HOME/.local/share/AppInstall/components/helpers/colors.sh
 
-echo -e "$green Which version of ruby do you use by default?  $close_color"
-read rubyversion
+rubyversion=$(dialog --title "Rails Install" --inputbox "Which version of ruby do you use by default? \n default version is: 2.5.1" 8 60 2>&1 >/dev/tty)
 
 if [ -z $rubyversion ]; then
   rubyversion=2.5.1
 fi
 
-echo -e "$green Installing required components! $close_color" &&
+echo -e "$green Installing required components! $close_color"
 sudo apt install -y build-essential curl gnupg2 autoconf bison libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm-dev libpq-dev ruby-dev &&
 
-echo -e "$green Installing RVM $close_color" &&
+echo -e "$green Installing RVM $close_color"
 gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB &&
 \curl -sSL https://get.rvm.io | bash -s stable &&
 
-echo -e "$green Installing Ruby $close_color" &&
+echo -e "$green Installing Ruby $close_color"
 rvm install $rubyversion &&
 
-echo -e "$green Installing Rails $close_color" &&
+echo -e "$green Installing Rails $close_color"
 sudo chown -R $(whoami) /var/lib/gems &&
 gem install rails &&
 gem install bundle &&
 
-echo -e "$green Do you want to install PostgreSql?$close_color [y/n]"
-read postgresql
+postgresql=$(dialog --title "Rails Install" --yesno "Do you want to install PostgreSql?" 8 60 2>&1 >/dev/tty)
 
-if [ $postgresql == "y" ]; then
+# if user press Yes
+if [ $? = 0 ]; then
   echo -e "$green Installing PostgreSql $close_color"
   sudo apt install -y postgresql postgresql-contrib libpq-dev
 fi
 
-echo -e "$green
-Ruby On Rails successfully installed 
-$close_color"
+dialog --clear --title 'Sucess:' --msgbox 'Ruby On Rails successfully installed' 6 40
