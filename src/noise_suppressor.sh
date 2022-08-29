ed338d22209ac373bc4b19203d9b80c95ef31883
Wilfison <wilfisonbatista@gmail.com>
2022-08-29 14:09:28 -0300

    fix config

--- src/noise_suppressor.sh
+++ src/noise_suppressor.sh
@@ -10,15 +10,15 @@ if [ ! -f "$pulse_bkp_file" ]; then
   cp $pulse_config_file $pulse_bkp_file
 fi
 
-pulse_config="### Enable Echo/Noise-Cancellation
-load-module module-echo-cancel use_master_format=1 aec_method=webrtc aec_args=\"analog_gain_control=0 digital_gain_control=1\" source_name=echoCancel_source sink_name=echoCancel_sink
-set-default-source echoCancel_source
-set-default-sink echoCancel_sink"
+pulse_filters_config='load-module module-echo-cancel aec_args="analog_gain_control=0 digital_gain_control=0" source_name=noiseless'
+pulse_filters_apply='set-default-source noiseless'
 
 show_info_log "Applying Settings"
-echo -e "\n$pulse_config" | sudo tee -a $pulse_config_file &&
+sudo sed -i "s/load-module module-filter-apply/load-module module-filter-apply\n$pulse_filters_config/" $pulse_config_file &&
+  sudo sed -i "s/#set-default-source input/$pulse_filters_apply/" $pulse_config_file &&
   pulseaudio -k &&
   pulseaudio --start &&
-  show_info_log "Noise Suppressor successfully installed!" &&
-  show_log "To restore the original settings run: sudo cp -f $pulse_bkp_file $pulse_config_file" &&
-  show_warning_log "You need to restart to apply settings"
+  show_info_log "Noise Suppressor successfully installed!"
+
+show_log "To restore the original settings run: sudo cp -f $pulse_bkp_file $pulse_config_file"
+show_warning_log "You need to restart to apply settings"
