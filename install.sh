cfacea8adcbf4b9a8a1dd4a3063dfb217681cbe1
Wilfison <wilfisonbatista@gmail.com>
2023-12-08 15:46:00 -0300

    Remove AppInstall.desktop and update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,39 @@
 #!/bin/bash
 
+set -e
+
 green="\033[0;32m"
 blue="\033[0;34m"
 close_color="\033[0m"
 
-echo -e "$green Installing essential tools.. $close_color" &&
-sudo apt install git -y &&
+echo -e "$green Installing essential tools.. $close_color"
+sudo apt install -y git
 
-echo -e "$green Downloading installer... $close_color" &&
+echo -e "$green Downloading installer... $close_color"
 mkdir -p $HOME/Apps
-cd $HOME/Apps &&
-rm -rf -f ./AppInstall
-sudo rm -f $HOME/.local/share/applications/AppInstall.desktop
 
-git clone https://github.com/Wilfison/AppsInstall AppInstall
+APP_PATH="$HOME/Apps/AppInstall"
 
-sudo cp ./AppInstall/AppInstall.desktop /usr/share/applications &&
-sudo ln -sf $HOME/Apps/AppInstall/AppInstall.sh /usr/bin/devinstall &&
+if [ -d "$APP_PATH" ]; then
+  cd $APP_PATH
+  git pull
+else
+  git clone https://github.com/wilfison/AppsInstall AppInstall
+  cd $APP_PATH
+fi
+
+sudo ln -sf $APP_PATH/AppInstall.sh /usr/bin/devinstall
+
+echo "[Desktop Entry]
+Version=1.3
+Name=App Install
+Comment=Installs various programs and prepares environments for programming without you typing codes.
+Exec=/usr/bin/devinstall
+Icon=$APP_PATH/AppInstall.svg
+Terminal=true
+Type=Application
+Categories=Development
+" >$HOME/.local/share/applications/AppInstall.desktop
 
 clear
 
